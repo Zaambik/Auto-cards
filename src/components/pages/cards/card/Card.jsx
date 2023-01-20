5cace2228e4e0fdebcac765304429c9ac969fc83
Zaambik <semenholodaev@gmail.com>
2023-01-20 16:22:10 +0300

    new styles for card

--- src/components/pages/cards/card/Card.jsx
+++ src/components/pages/cards/card/Card.jsx
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-
-import imgCard1 from '../img/card1.png'
-import imgCard2 from '../img/card2.png'
-import imgCard3 from '../img/card3.png'
-import styles from './Card.module.scss'
-
-
-const Cards = ({img, h, text}) => {
-
-  return (
-    <div className={ styles.card }>
-        <img
-            src={ imgCard1 }
-            alt="INVALID CONTENT(Изображение не загружено)"
-            width="400px"
-        />
-        <div className={ styles.cardText }>
-            <h3 className="card-title">
-            Dark forest</h3>
-            <p className="card-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum doloribus inventore eligendi in, ea, sapiente adipisci dolores earum soluta nam aperiam doloremque ratione, possimus labore ab quasi architecto fugiat necessitatibus.
-            </p>
-            <button href="#" className={ styles.cardBtn }>Look this art</button>
-        </div>
-    </div>
-  )
-};
-
+import { useState } from 'react' 
+ 
+import imgCard1 from '../img/card1.png' 
+import imgCard2 from '../img/card2.png' 
+import imgCard3 from '../img/card3.png' 
+import styles from './Card.module.scss' 
+ 
+ 
+const Cards = ({img, h, text}) => { 
+ 
+  return ( 
+    <div className={ styles.card }> 
+      <div className={ styles.imgWrapper }><img 
+        src={ imgCard1 } 
+        alt="Img Lake" 
+        width="400px" 
+      /> </div>
+      <div className={ styles.cardText }> 
+          <h2>Dark forest</h2> 
+          <div>
+            <p> 
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum doloribus inventore eligendi in, ea, sapiente adipisci dolores earum soluta nam aperiam doloremque ratione, possimus labore ab quasi architecto fugiat necessitatibus. 
+            </p> 
+            <a href="#">Look this art</a>
+          </div> 
+      </div> 
+    </div> 
+  ) 
+}; 
+ 
 export default Cards
